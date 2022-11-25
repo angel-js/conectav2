e1077eb6518476346aa213b47f99e223d82fc5a7
angel-js <angelserrano143@gmail.com>
2022-11-25 13:10:04 -0300

    Falta arreglar las tablas sintoma y patología con tablas pivote

--- main_app/models.py
+++ main_app/models.py
@@ -9,6 +9,15 @@ class Region(models.Model):
         managed = False
         db_table = 'region'
 
+class Comuna(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_region = models.CharField(max_length=35)
+    id_region = models.OneToOneField('Region', models.DO_NOTHING, db_column='id_region')
+
+    class Meta:
+        managed = False
+        db_table = 'comuna'
+
 
 class Rol(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
@@ -33,7 +42,8 @@ class Persona(models.Model):
     email = models.CharField(max_length=80)
     contrasenia = models.CharField(max_length=80)
     id_rol = models.ForeignKey(Rol, on_delete=models.CASCADE, db_column='id_rol', default=3)
-    id_comuna = models.OneToOneField(Comuna, models.DO_NOTHING, db_column='id_comuna')
+    id_region = models.OneToOneField('Region', models.DO_NOTHING, db_column='id_region')
+    id_comuna = models.OneToOneField('Comuna', models.DO_NOTHING, db_column='id_comuna')
 
     class Meta:
         managed = False
@@ -53,15 +63,40 @@ class Usuario(Persona):
 class Paciente(Persona):
     id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
     sexo_biologico = models.CharField(max_length=20)
-    id_usuario = models.OneToOneField('Usuario', models.DO_NOTHING, db_column='id_usuario')
-    
-    id_sintoma = models.OneToOneField('Sintomas', models.DO_NOTHING, db_column='id_sintoma')
+    id_patologia = models.ManyToOne('Patologia', models.DO_NOTHING, null=True)
 
     class Meta:
         managed = False
         db_table = 'paciente'
 
-class Familiar(models.Model):
+class Patologia(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_patologia = models.CharField(max_length=35)
+
+    class Meta:
+        managed = False
+        db_table = 'patologia'
+
+class Ingreso(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    fecha_comentario = models.DateField()
+    hora_comentario = models.TimeField()
+    id_paciente = models.ForeignKey('Paciente', models.DO_NOTHING, db_column='id_paciente')
+    id_sintoma = models.ManyToOne('Sintoma', models.DO_NOTHING, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ingreso'
+
+class Sintoma(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    nombre_sintoma = models.CharField(max_length=35)
+
+    class Meta:
+        managed = False
+        db_table = 'sintomas'
+
+class Familiar(Usuario):
     id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
     relacion_paciente = models.CharField(max_length=30)
 
@@ -70,10 +105,33 @@ class Familiar(models.Model):
         db_table = 'familiar'
 
 
-class Funcionario(models.Model):
+class Funcionario(Usuario):
     id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
     cargo = models.CharField(max_length=30)
 
     class Meta:
         managed = False
         db_table = 'funcionario'
+
+
+class Ficha(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    id_comentario = models.ForeignKey(Comentario, models.DO_NOTHING, db_column='id_comentario')
+    id_ingreso = models.ForeignKey('Ingreso', models.DO_NOTHING, db_column='id_ingresos')
+    id_paciente = models.ForeignKey('Paciente',  models.DO_NOTHING, db_column='id_paciente', unique=True )
+
+    class Meta:
+        managed = False
+        db_table = 'ficha'
+
+class Comentario(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    fecha_comentario = models.DateField()
+    hora_comentario = models.TimeField()
+    estado = models.CharField(db_column='Estado', max_length=20)  # Field name made lowercase.
+    comentario = models.CharField(db_column='Comentario', max_length=300)  # Field name made lowercase.
+    id_funcionario = models.ForeignKey('Funcionario', models.DO_NOTHING, db_column='id_funcionario')
+
+    class Meta:
+        managed = False
+        db_table = 'comentario'
